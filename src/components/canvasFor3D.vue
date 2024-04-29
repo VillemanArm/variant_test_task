2ca8a2983800764289aa382310e91ec74a622369
Villeman <villeman.arm@gmail.com>
2024-04-29 19:01:45 +0300

    refactor: removed the extra code from onMount hook and added functions addMesh and addTexture

--- src/components/canvasFor3D.vue
+++ src/components/canvasFor3D.vue
@@ -8,95 +8,104 @@ import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 // }>();
 
 
-const cubeCanvas = ref<HTMLInputElement>() // элемент, в котором будет отображаться 3D элемент
+const canvas = ref<HTMLInputElement>() // элемент, в котором будет отображаться 3D элемент
+const currentMesh = ref<THREE.Mesh>()
 
-onMounted(() => {
-	const scene = new THREE.Scene(); // создание сцены
-	scene.add(new THREE.GridHelper( 20, 20, 0x707a87, 0xffffff ))
+const scene = new THREE.Scene();
 
-	const axesHelper = new THREE.AxesHelper( 2 ); // создание объекта, показыващего оси. Принимает длину осей
-	scene.add( axesHelper)
-	
-	const renderer = new THREE.WebGLRenderer({ canvas: cubeCanvas.value}); // создание отрисовщика
+const gridHelper = new THREE.GridHelper( 20, 20, 0x707a87, 0xffffff )
+scene.add(gridHelper)
+
+const axesHelper = new THREE.AxesHelper( 2 ); // создание объекта, показыващего оси. Принимает длину осей
+scene.add( axesHelper)
+
+const camera = new THREE.PerspectiveCamera( 70, (canvas.value?.offsetWidth || window.innerWidth) / (canvas.value?.offsetHeight || window.innerHeight)); // создание камеры. передаем угол обзора в градусах и соотношение сторон
+camera.position.set(3, 3, 3) // установка позиции камеры x, y, z
+camera.lookAt(scene.position); // направление камеры в центр сцены
+scene.add( camera )
+
+const light = new THREE.DirectionalLight(0xffffff, 1); // создание света
+light.position.set(5, 4, 2);
+scene.add(light)
+
+let renderer: THREE.WebGLRenderer
+
+const createRenderer = () => {
+	renderer = new THREE.WebGLRenderer({ canvas: canvas.value}); // создание отрисовщика
 	renderer.setClearColor('grey') // установка цвета фона
-	renderer.setSize( cubeCanvas.value?.offsetWidth || window.innerWidth, cubeCanvas.value?.offsetHeight || window.innerHeight) // установка размера рендерера
-	
-	// const geometry = new THREE.BoxGeometry( 1, 1, 1 ); // создание объекта
-	// const material = new THREE.MeshStandardMaterial( { color: 'green'} ); // на этом материале отображается изменение цвета
-	// const cube = new THREE.Mesh( geometry, material );
-	// scene.add( cube );
-	// scene.add(cube);
-	
-	const camera = new THREE.PerspectiveCamera( 70, (cubeCanvas.value?.offsetWidth || window.innerWidth) / (cubeCanvas.value?.offsetHeight || window.innerHeight)); // создание камеры. передаем угол обзора в градусах и соотношение сторон
-	camera.position.set(3, 3, 3) // установка позиции камеры x, y, z
-	camera.lookAt(scene.position); // направление камеры в центр сцены
-	scene.add( camera )
-	
-	const light = new THREE.DirectionalLight(0xffffff, 1); // создание света
-	light.position.set(5, 4, 2);
-	scene.add(light)
-	
-	const loader = new GLTFLoader();
-	loader.load(
-		'/mashes/geometries/cube.glb', 
-		(gltf) => {
-			// console.log('success');		
-			// console.log(gltf.scene.children[0]);
-			const cube = gltf.scene.children[0]
-			cube.scale.set(2, 2, 2)
-			// cube.position.set(0, 2, 0)
-			// cube.material =  new THREE.MeshStandardMaterial( { color: 'green'} )
-			const textureLoader = new THREE.TextureLoader();
-			const texture = textureLoader.load(
-				'/mashes/textures/albedo/albedo-wood.png',
-				(texture) => {
-					cube.material =  new THREE.MeshStandardMaterial( { map: texture} )
-					renderer.render(scene, camera)
-				},
-				undefined,
-				(error) => {
-					console.log(error);
-				}
-			
-				);
+	renderer.setSize( canvas.value?.offsetWidth || window.innerWidth, canvas.value?.offsetHeight || window.innerHeight) // установка размера рендерера
+}
 
-			scene.add(cube)
+// const renderScene = (): void => {
+// 	renderer.render(scene, camera)
+// 	requestAnimationFrame(renderScene)
+	// cube.rotation.x += 0.01
+	// cube.rotation.y += 0.01
+// }
+
+// renderScene()
+
+const meshLoader = new GLTFLoader();
+
+const addMesh = (meshPath: string) => { //'/meshes/geometries/cube.glb'
+	meshLoader.load(
+		meshPath, 
+		(gltf) => {
+			const mesh: THREE.Object3D = gltf.scene.children[0]
+			scene.add(mesh)
 			renderer.render(scene, camera)
+			currentMesh.value = mesh as THREE.Mesh
 		},
 		undefined,
 		(error) => {
-			console.log(error);
-			
+			console.log(error);			
 		}
-
 	)
+}
 
-	//renderer.render(scene, camera) // запуск отрисовки, делается после создания всех объектов
+const textureLoader = new THREE.TextureLoader();
+
+const addTexture = (texturePath: string) => { //'/meshes/textures/albedo/albedo-wood.png'
+	textureLoader.load(
+	texturePath,
+	(texture) => {
+		if (currentMesh.value) {
+			currentMesh.value.material =  new THREE.MeshStandardMaterial( { map: texture} )
+			renderer.render(scene, camera)
+		}
+	},
+	undefined,
+	(error) => {
+		console.log(error);
+	}
+	)
+}
+
+// watch(meshes, () => {
+// 	console.log(meshes);
+// 	console.log('meshes changed');
 	
+// 	// scene.add(meshes.value[meshes.value.length - 1])
+// 	// 	const geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 ); // создание объекта
+// 	// const material = new THREE.MeshStandardMaterial( { color: 'green'} ); // на этом материале отображается изменение цвета
+// 	// const cube = new THREE.Mesh( geometry, material );
+// 	// scene.add( cube );
+// 	// renderer.render(scene, camera)
 	
+// }, {deep: true})
 
-	// const animate = (): void => {
-	// 	renderer.render(scene, camera)
-	// 	requestAnimationFrame(animate)
-	// 	cube.rotation.x += 0.01
-	// 	cube.rotation.y += 0.01
-	// }
+onMounted(() => {
+	createRenderer()
+	renderer.render(scene, camera)
 
-	// animate()
+	addMesh('/meshes/geometries/cube.glb')
 
 })
-
 
 </script>
 
 <template>
-
-	<canvas ref="cubeCanvas" class="cubeCanvas">
-		
-	</canvas>
-
-
-
+	<canvas ref="canvas" class="canvas"></canvas>
 </template>
 
 <style lang="sass">
@@ -104,7 +113,7 @@ onMounted(() => {
 	body
 		overflow: hidden
 
-	.cubeCanvas
+	.canvas
 		width: 100vw
 		height: 100vh
 
